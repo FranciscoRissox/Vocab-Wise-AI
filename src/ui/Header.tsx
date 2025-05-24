2901130848bbb0072b467f3342e0caea524050c5
francisco risso <frisso2001@gmail.com>
2025-05-24 17:50:08 -0300

    Add link on header icon to landing

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -2,8 +2,10 @@ import { UserMenu } from "./UserMenu";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from "../firebase/auth";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
+  const navigate = useNavigate();
   const [hasUser, setHasUser] = useState(false);
 
   useEffect(() => {
@@ -15,7 +17,10 @@ export function Header() {
 
   return (
     <header className="w-full fixed top-0 left-0  px-6 py-4 bg-white shadow-md flex items-center justify-between">
-      <span className="text-2xl font-bold text-blue-600 tracking-tight">
+      <span
+        className="text-2xl font-bold text-blue-600 tracking-tight select-none"
+        onClick={() => navigate("/")}
+      >
         Vocab<span className="text-gray-800">Wise</span>
         <span className="text-green-600">AI</span>
       </span>
